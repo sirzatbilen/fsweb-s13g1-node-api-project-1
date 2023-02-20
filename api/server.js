96c8247062ac2af1ec149a9d0ae95f297eaf2133
Put your Name in Here <93521133+sirzatbilen@users.noreply.github.com>
2023-02-20 22:57:10 +0300

    S13G1 TAMAMLANDI

--- api/server.js
+++ api/server.js
@@ -1,3 +1,93 @@
 // SUNUCUYU BU DOSYAYA KURUN
 
-module.exports = {}; // SERVERINIZI EXPORT EDİN {}
+const express = require("express");
+const User = require("./users/model");
+
+const server = express();
+server.use(express.json());
+
+//POST Api ( POST kullanmamızın : cevabı bizi ilgilendiriyor)
+server.post("/api/users", (req, res) => {
+  let user = req.body;
+  if (!user.name || !user.bio) {
+    res
+      .status(400)
+      .json({ message: "Lütfen kullanıcı için bir name ve bio sağlayın" });
+  } else {
+    User.insert(user)
+      .then((newUser) => {
+        res.status(201).json(newUser);
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .json({ message: "Veritabanına kaydedilirken bir hata oluştu" });
+      });
+  }
+});
+
+server.get("/api/users", (req, res) => {
+  User.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Kullanıcı bilgileri alınamadı" });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: "Belirtilen ID'li kullanıcı bulunamadı" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Kullanıcı bilgisi alınamadı" });
+    });
+});
+
+server.delete("/api/users/:id", async (req, res) => {
+  try {
+    let willBeDeleteUser = await User.findById(req.params.id);
+    if (!willBeDeleteUser) {
+      res
+        .status(404)
+        .json({ message: "Belirtilen ID li kullanıcı bulunamadı" });
+    } else {
+      await User.remove(req.params.id);
+      res.status(200).json(willBeDeleteUser);
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Kullanıcı silinemedi" });
+  }
+});
+
+server.put("/api/users/:id", async (req, res) => {
+  try {
+    let willBeUpdateUser = await User.findById(req.params.id);
+    if (!willBeUpdateUser) {
+      res
+        .status(404)
+        .json({ message: "Belirtilen ID'li kullanıcı bulunamadı" });
+    } else {
+      if (!req.body.name || !req.body.bio) {
+        res
+          .status(400)
+          .json({ message: "Lütfen kullanıcı için name ve bio sağlayın" });
+      } else {
+        let updateUser = await User.update(req.params.id, req.body);
+        res.status(200).json(updateUser);
+      }
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Kullanıcı bilgileri güncellenemedi" });
+  }
+});
+
+module.exports = server; // SERVERINIZI EXPORT EDİN {}
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const server = require('./api/server');
+const server = require("./api/server");
 
 const port = 9000;
+
+server.listen(port, () => {
+  console.log("Sunucu 8000 numaralı porta bağlandı.");
+});
 
 // START YOUR SERVER HERE
